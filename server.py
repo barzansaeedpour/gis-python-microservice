b146cabc96c29375eea8358233cf1e91c4b683f1
barzansaeedpour <barzansaeedpour@gmail.com>
2025-01-20 11:37:23 +0330

    working on new version with crop, translate and zoom

--- server.py
+++ server.py
@@ -4,7 +4,8 @@ import dwg_pb2
 import dwg_pb2_grpc
 import subprocess
 import geopandas as gpd
-from shapely.geometry import box
+from shapely.geometry import box, Point
+from shapely.affinity import translate, scale
 import os
 
 class FileService(dwg_pb2_grpc.FileServiceServicer):
@@ -24,18 +25,58 @@ class FileService(dwg_pb2_grpc.FileServiceServicer):
         RECURSIVE = "0"
         AUDIT = "1"
         INPUTFILTER = "*.DWG"
+        # cmd = [TEIGHA_PATH, INPUT_FOLDER, OUTPUT_FOLDER, OUTVER, OUTFORMAT, RECURSIVE, AUDIT, INPUTFILTER]
+        # subprocess.run(cmd, shell=True)
+        # data = gpd.read_file("./output/file.dxf")
+        # data['geom_type'] = data.geometry.type
+        # data.set_crs(epsg=4326, inplace=True)
+        # bounding_box_polygon = box(637000, 3902000, 672000, 3919000)
+        # cropped_data = data[data.geometry.intersects(bounding_box_polygon)]
+        # data_lines = cropped_data[cropped_data['geom_type'] == 'LineString']
+        # data_points = cropped_data[cropped_data['geom_type'] == 'Point']
+        # data_lines.to_file("./output/cropped_lines.shp")
+        # data_points.to_file("./output/cropped_points.shp")
+        
+        # Convert DWG to DXF using ODA File Converter
         cmd = [TEIGHA_PATH, INPUT_FOLDER, OUTPUT_FOLDER, OUTVER, OUTFORMAT, RECURSIVE, AUDIT, INPUTFILTER]
         subprocess.run(cmd, shell=True)
-        data = gpd.read_file("./output/file.dxf")
+
+        # Load the converted DXF file using GeoPandas
+        data = gpd.read_file("./output/Khoroseh Var-Plan & Profile.dxf")
         data['geom_type'] = data.geometry.type
-        data.set_crs(epsg=4326, inplace=True)
+        data.set_crs(epsg=3857, inplace=True)
+
+        # Calculate the bounding box and crop the data
+        bounding_box = data.total_bounds  # Returns (minx, miny, maxx, maxy)
+        print(f"Bounding Box: {bounding_box}")
         bounding_box_polygon = box(637000, 3902000, 672000, 3919000)
         cropped_data = data[data.geometry.intersects(bounding_box_polygon)]
+
+        # Translate the cropped data to the new center coordinates (5715364, 4261022)
         data_lines = cropped_data[cropped_data['geom_type'] == 'LineString']
         data_points = cropped_data[cropped_data['geom_type'] == 'Point']
+
+        current_center_x, current_center_y = (bounding_box[0] + bounding_box[2]) / 2, (bounding_box[1] + bounding_box[3]) / 2
+        new_center_x, new_center_y = 5715364, 4261022
+        delta_x, delta_y = new_center_x - current_center_x, new_center_y - current_center_y
+
+        data_lines['geometry'] = data_lines['geometry'].apply(lambda geom: translate(geom, xoff=delta_x, yoff=delta_y))
+        data_points['geometry'] = data_points['geometry'].apply(lambda geom: translate(geom, xoff=delta_x, yoff=delta_y))
+
+        # Scale the geometries around the new center
+        scale_factor = 3  # >1 for zooming in, <1 for zooming out
+        center_point = Point(new_center_x, new_center_y)
+        data_lines['geometry'] = data_lines['geometry'].apply(lambda geom: scale(geom, xfact=scale_factor, yfact=scale_factor, origin=center_point))
+        data_points['geometry'] = data_points['geometry'].apply(lambda geom: scale(geom, xfact=scale_factor, yfact=scale_factor, origin=center_point))
+
+        # Save the shapefiles
+        # data_lines.to_file("./output/cropped_lines_stretched.shp")
+        # data_points.to_file("./output/cropped_points_stretched.shp")
         data_lines.to_file("./output/cropped_lines.shp")
         data_points.to_file("./output/cropped_points.shp")
-
+        
+        
+        
         # Gather the converted files
         file_names = [
             "cropped_lines.cpg", "cropped_lines.dbf", "cropped_lines.prj", "cropped_lines.shp", "cropped_lines.shx",
